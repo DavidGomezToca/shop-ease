915bdfeab3505048b28a8b3fa33dd9eda53f1aa2
DavidGomezToca <DAVID_AGOMEZT@HOTMAIL.COM>
2025-10-08 12:56:58 +0200

    SHOP EASE 0.14.0
    Add Form View Product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\ProductType;
 
 final class ProductController extends AbstractController
 {
@@ -15,6 +19,20 @@ final class ProductController extends AbstractController
         $products = $productRepository->findAll();
         return $this->render('admin/product/index.html.twig', [
             'products' => $products,
+        ]);
+    }
+    #[Route('admin/add/product', name: 'app_product_add')]
+    public function new(Request $request, EntityManagerInterface $entityManager)
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product->setCreatedAt(new \DateTimeImmutable());
+        }
+
+        return $this->render('admin/product/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
