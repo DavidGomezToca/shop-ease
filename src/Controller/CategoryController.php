ff391c11041128134ab9b09215ab8a567213f55c
DavidGomezToca <DAVID_AGOMEZT@HOTMAIL.COM>
2025-10-13 16:42:51 +0200

    SHOP EASE 0.21.0
    Add Form Category Type

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,5 +20,17 @@ final class CategoryController extends AbstractController
         return $this->render('admin/category/index.html.twig', [
             'categories' => $categories,
         ]);
+    }
+    #[Route('admin/add/category', name: 'app_category_add')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($category);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_category_add');
+        }
+        return $this->render('admin/category/new.html.twig');
     }
 }
